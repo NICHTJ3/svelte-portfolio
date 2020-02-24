8ac4bde1cf489b295d6f206071657a0e496a0b4f
NICHTJ3 <nicholsontrent@gmail.com>
2020-02-24 19:50:49 +1300

    Add basic nav bar using bootstrap

---
+++ src/components/nav/nav.svelte
@@ -0,0 +1,44 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<nav
+  class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
+  id="sideNav">
+  <a class="navbar-brand js-scroll-trigger" href="#page-top">
+    <span class="d-block d-lg-none">Trent Nicholson</span>
+    <span class="d-none d-lg-block">
+      <img
+        class="img-fluid img-profile rounded-circle mx-auto mb-2"
+        src="https://avatars3.githubusercontent.com/u/39316919?s=400&u=02f9c31e01ffd14ec0dec546876a99c53f6ef4a9&v=4"
+        alt="" />
+    </span>
+  </a>
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent"
+    aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon" />
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="#home">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+      </li>
+    </ul>
+  </div>
+</nav>
